5acc7dff3deb930313a1bad680c041ec0839c4cf
Dmytro_Stekanov <dstekanov.tech@gmail.com>
2020-03-06 10:16:15 +0200

    add video.js, used addCommand

---
+++ tests/video.js
@@ -0,0 +1,23 @@
+browser.addCommand('isVideoPaused', () => {
+    return browser.execute(() => {
+        var video = document.querySelector('#dance-video');
+        return video.paused;
+    })
+}); 
+
+describe('About Us video', () => {
+    beforeEach( () => {
+        browser.url('/');
+        $('=About Us').click();
+    })
+
+    it('should open the modal with video paused', () => {
+        expect(browser.isVideoPaused()).to.be.true;
+    })
+
+    it('play video on "play" click', () => {
+        $('#play-btn').click();
+        expect(browser.isVideoPaused()).to.be.false;
+    })
+
+})
