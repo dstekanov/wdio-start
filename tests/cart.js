8462a876ab0bf418aa7697d4bec3233d71d78f18
Dmytro_Stekanov <dstekanov.tech@gmail.com>
2020-03-07 07:34:40 +0200

    add getter for cart tests

--- tests/cart.js
+++ tests/cart.js
@@ -1,39 +1,46 @@
 describe('Cart Functionality', () => {
     var btn = "#buyNowButton";
     var qty = "#qty";
+    var thankYou = ".callout*=Thank you human";
+
+    var cart = {
+        get btn() { return $(btn); },
+        get qty() { return $(qty); },
+        get thankYou() { return $(thankYou); }
+    }
+
     beforeEach(() => {
         browser.url('/product-page.html');
     })
 
     it('should only let you buy after setting quantity', () => {
-        expect($(btn).isEnabled(), "'but now' should be disabled for begin").to.be.false;
-        $(qty).setValue(10);
-        expect($(btn).isEnabled(), "'but now' is now enabled").to.be.true;
+        expect(cart.btn.isEnabled(), "'but now' should be disabled for begin").to.be.false;
+        cart.qty.setValue(10);
+        expect(cart.btn.isEnabled(), "'but now' is now enabled").to.be.true;
     })
 
     describe('checkout process', () => {
         beforeEach(() => {
-            $(qty).setValue(10);
-            $(btn).click();
+            cart.qty.setValue(10);
+            cart.btn.click();
         })
     
         it('should disable buy now button during processing', () => {
-            expect($(btn).isEnabled(), "'but now' should be disabled after clicking").to.be.false;
-            expect($(btn).getText(), "Verify 'but now' text has changed").to.contain("Purchasing");
+            expect(cart.btn.isEnabled(), "'but now' should be disabled after clicking").to.be.false;
+            expect(cart.btn.getText(), "Verify 'but now' text has changed").to.contain("Purchasing");
         })
 
         it('should show a thank you message with qty and type', () => {
-            var thankYou = ".callout*=Thank you human";
-            $(thankYou).waitForExist(3000);
-            expect($(thankYou).getText()).to.contain("10 T-800 Model 101");
+            cart.thankYou.waitForExist(3000);
+            expect(cart.thankYou.getText()).to.contain("10 T-800 Model 101");
         })
         
         it('should reset button text after purchasing complete', () => {
             browser.waitUntil(() => {
-                return $(btn).getText() !== 'Purchasing...'
+                return cart.btn.getText() !== 'Purchasing...'
             }, 3000);
 
-            expect($(btn).getText()).to.contain("Buy Now");
+            expect(cart.btn.getText()).to.contain("Buy Now");
         })
     })
 })
